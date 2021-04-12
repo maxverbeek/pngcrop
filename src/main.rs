4560c1894998df015dbd3126419ee63823d85691
Max Verbeek <m4xv3rb33k@gmail.com>
2021-04-12 21:52:28 +0200

    feat: add support for background removal for non-transparent backgrounds

--- src/main.rs
+++ src/main.rs
@@ -76,12 +76,7 @@ fn process_file(path: &String, dest: &String) -> ImageResult<()> {
     println!("{}", path);
 
     let img = ImageReader::open(path)?.decode()?;
-    let has_alpha = img.color().has_alpha();
     
-    if !has_alpha {
-        return Ok(());
-    }
-
     let canvas = img.into_rgba8();
 
     let bounds = find_boundaries(&canvas);
@@ -98,10 +93,35 @@ fn find_boundaries(img: &image::RgbaImage) -> ContentBounds {
     };
 
     const ALPHA_IDX: usize = 3;
+    
+    let topleft = img.get_pixel(0, 0);
+    let botrght = img.get_pixel(img.width() - 1, img.height() - 1);
+
+    // select a pixel that represents the background of the image.
+    // this must be the topleft or bottomright pixel, since if it is
+    // not one of those pixels, the picture cannot be cropped further.
+    // if the pixels are not the same, it is ambiguous what the background
+    // of the image is, unless one of those pixels is transparent.
+    let sample = {
+        if topleft == botrght {
+            topleft
+        } else if topleft[ALPHA_IDX] == 0 {
+            topleft
+        } else if botrght[ALPHA_IDX] == 0 {
+            botrght
+        } else {
+            // nothing to be done, ambiguous what the background is
+            bounds.minx = 0;
+            bounds.maxx = img.width() - 1;
+            bounds.miny = 0;
+            bounds.maxy = img.height() - 1;
+            return bounds;
+        }
+    };
 
     for (x, y, pixel) in img.enumerate_pixels() {
-        if pixel[ALPHA_IDX] != 0 {
-            // get boundary coordinates for non-alpha pixels.
+        if pixel != sample {
+            // get boundary coordinates for non-background pixels.
             // can be optimized with enumerate_row and smart skipping.
             bounds.minx = min(bounds.minx, x);
             bounds.maxx = max(bounds.maxx, x);
